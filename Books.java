ea1ea6e2ba9725909bccf3a6c3da2104f1165194
Zapihra <iida.vaaja@gmail.com>
2024-10-06 18:23:43 +0300

    remembering the basics

---
+++ Books.java
@@ -0,0 +1,11 @@
+public class Books {
+    private String book = "grannys nook";
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String newBook) {
+        this.book = newBook;
+    }
+}
